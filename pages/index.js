64690ddac85397147468cccfe16ade125f925554
claudiopome <claudiopome@gmail.com>
2024-04-06 16:27:12 +0200

    query hp.

--- pages/index.js
+++ pages/index.js
@@ -5,29 +5,27 @@ import { BlockRenderer } from "components/BlockRenderer";
 
 export default function Home(props) {
   console.log("Props:", props);
-  return <div>
-    <BlockRenderer blocks={props.blocks} />
-  </div>;
+  return <div>Next JS &amp; WordPress Course</div>;
 }
 
 export const getStaticProps = async () => {
-  const {data} = await client.query({
+  const { data } = await client.query({
     query: gql`
-    query NewQuery {
-      nodeByUri(uri: "/") {
-        ... on Page {
-          id
-          title
-          blocksJSON
+      query NewQuery {
+        nodeByUri(uri: "/") {
+          ... on Page {
+            id
+            blocks(postTemplate: false)
+          }
         }
       }
-    }
-    `
-  })
-  const blocks = CleanAndTransformBlocks(data.nodeByUri.blocksJSON);
+    `,
+  });
+
   return {
     props: {
-      blocks
+      blocks: data.nodeByUri.blocks,
+      myexampleprop: "test",
     },
-  }
-}
+  };
+};
